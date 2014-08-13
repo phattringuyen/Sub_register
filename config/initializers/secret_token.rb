3b05929de7bcc2adeea9f23a29dd6aaa7be72c81
PN <pn@ubuntu.ubuntu-domain>
2014-08-13 19:58:45 +0700

    Pingo_Lab

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SubReg::Application.config.secret_key_base = '7c72c9d6c64619911f53f4a0ecb3bcb557663b7cbc206c96935d5de82bce61e400f662111627039cef39918f587b8c366ef9c45a452e59ff41ef2e0e62d95192'
